a29c693937e83fbd5650068c12c37e6dd97f7d5d
KASU KIBANGU BENI <114804167+benikib@users.noreply.github.com>
2024-07-03 20:10:09 +0200

    Update pag.blade.php

--- resources/views/layouts/pag.blade.php
+++ resources/views/layouts/pag.blade.php
@@ -22,7 +22,7 @@
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.min.css')}}" />
+        <link rel="stylesheet" type="text/css" href="https://main--profound-tiramisu-fb2889.netlify.app/css/style.min.css" />
 
 
 
