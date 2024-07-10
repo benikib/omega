d8be4863c56c57693174ccbf0b99a9a9fa3e4e80
KASU KIBANGU BENI <114804167+benikib@users.noreply.github.com>
2024-07-10 14:54:35 +0200

    Update 2023_11_01_225332_create_notes_table.php

--- database/migrations/2023_11_01_225332_create_notes_table.php
+++ database/migrations/2023_11_01_225332_create_notes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notes', function (Blueprint $table) {
+        Schema::create('notess', function (Blueprint $table) {
             $table->id();
             
         });
