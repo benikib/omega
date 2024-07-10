2e88261719c546c885c19f6668c4df9a28f8eed1
KASU KIBANGU BENI <114804167+benikib@users.noreply.github.com>
2024-07-10 15:11:46 +0200

    Update 2024_06_18_152146_create_questionnaires_table.php

--- database/migrations/2024_06_18_152146_create_questionnaires_table.php
+++ database/migrations/2024_06_18_152146_create_questionnaires_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('questionnaires', function (Blueprint $table) {
+        Schema::create('questionnaire', function (Blueprint $table) {
             $table->id();
             $table->json('question');
             
